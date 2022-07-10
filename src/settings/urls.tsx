28fbde97383a3e2575fe2af23e4c7aef83c7191d
DR497 <47689875+dr497@users.noreply.github.com>
2022-07-10 23:10:26 +0800

    remove `/#/` and upgrade deps

--- src/settings/urls.tsx
+++ src/settings/urls.tsx
@@ -2,7 +2,7 @@ const Urls = {
   twitter: "https://twitter.com/bonfida",
   github: "https://github.com/Bonfida",
   website: "https://bonfida.org",
-  naming: "https://naming.bonfida.org/#/domain/",
+  naming: "https://naming.bonfida.org/domain/",
   solscanAddress: "https://solscan.io/account/",
   solscanTx: "https://solscan.io/tx/",
 };
